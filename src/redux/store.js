d5fdf2915b8bcc46958543d99aea574387e73bad
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2021-12-08 12:29:41 +0100

    solving a bug tu update and delete

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,11 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-const middlewares = [thunk];
+const middlewares = [thunk, logger];
 
-if (process.env.NODE_ENV === "development") {
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV === "development") {
+//   middlewares.push(logger);
+// }
 
 export const store = createStore(reducers, applyMiddleware(...middlewares));
 
